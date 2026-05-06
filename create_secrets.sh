97d52736d4d49d6ee1afab2f08f9eb05133f1982
Linas Z <margarinas@gmail.com>
2026-05-06 13:42:55 +0200

    pause pxc

--- create_secrets.sh
+++ create_secrets.sh
@@ -2,6 +2,9 @@
 
 # Set the primary search directory (default is ./apps)
 SEARCH_DIR="${1:-./apps}"
+KUBE_CONTEXT="${KUBE_CONTEXT:-velocita}"
+NAMESPACE="${NAMESPACE:-default}"
+SEALED_SECRETS_NAMESPACE="${SEALED_SECRETS_NAMESPACE:-infrastructure}"
 
 # Additional directory to scan
 
@@ -13,11 +16,10 @@ SEARCH_DIRS=("$SEARCH_DIR")
 
 seal_env_file() {
     local env_file="$1"
-    local dir_path secret_name namespace
+    local dir_path secret_name
 
     dir_path=$(dirname "$env_file")
     secret_name=$(basename "$dir_path")
-    namespace="default"
 
     case "$secret_name" in
       forgejo)
@@ -25,9 +27,48 @@ seal_env_file() {
         ;;
     esac
 
-    kubectl --context k3s -n "$namespace" create secret generic "$secret_name" \
+    kubectl --context "$KUBE_CONTEXT" -n "$NAMESPACE" create secret generic "$secret_name" \
       -o yaml --from-env-file "$env_file" --dry-run=client \
-      | kubeseal --context k3s --controller-namespace=infrastructure -o yaml > "$dir_path"/sealedSecret.yaml
+      | kubeseal --context "$KUBE_CONTEXT" --controller-namespace="$SEALED_SECRETS_NAMESPACE" -o yaml > "$dir_path"/sealedSecret.yaml
+}
+
+read_env_value() {
+    local env_file="$1"
+    local key="$2"
+
+    awk -F= -v key="$key" '
+        $1 == key {
+            value = substr($0, length($1) + 2)
+            if ((value ~ /^".*"$/) || (value ~ /^\047.*\047$/)) {
+                value = substr(value, 2, length(value) - 2)
+            }
+            print value
+            exit
+        }
+    ' "$env_file"
+}
+
+seal_postgres_credentials() {
+    local env_file="$1"
+    local dir_path app_name secret_name username password
+
+    dir_path=$(dirname "$env_file")
+    app_name=$(basename "$dir_path")
+    secret_name="$app_name-postgres-credentials"
+    username="$app_name-db-user"
+    password=$(read_env_value "$env_file" "DB_PASSWORD")
+
+    if [ -z "$password" ]; then
+        return
+    fi
+
+    kubectl --context "$KUBE_CONTEXT" -n "$NAMESPACE" create secret generic "$secret_name" \
+      --type=kubernetes.io/basic-auth \
+      --from-literal=username="$username" \
+      --from-literal=password="$password" \
+      --dry-run=client -o yaml \
+      | kubectl label --local -f - cnpg.io/reload=true -o yaml \
+      | kubeseal --context "$KUBE_CONTEXT" --controller-namespace="$SEALED_SECRETS_NAMESPACE" -o yaml > "$dir_path"/postgres-sealedSecret.yaml
 }
 
 seal_file_secrets() {
@@ -75,7 +116,7 @@ seal_file_secrets() {
             echo "---" >> "$output_file"
         fi
 
-        kubeseal --context k3s --controller-namespace=infrastructure -o yaml < "$doc" >> "$output_file"
+        kubeseal --context "$KUBE_CONTEXT" --controller-namespace="$SEALED_SECRETS_NAMESPACE" -o yaml < "$doc" >> "$output_file"
         first_doc=0
         count=$((count + 1))
     done
@@ -92,6 +133,7 @@ seal_file_secrets() {
 find "${SEARCH_DIRS[@]}" -type f -name ".env" | while read -r env_file; do
     echo "$EXECUTE_COMMAND $env_file"
     seal_env_file "$env_file"
+    seal_postgres_credentials "$env_file"
 done
 
 # Find and process file-based secret manifests (supports both common and typo filename)
